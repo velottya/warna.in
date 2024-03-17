b4ba3bf209f4f3940644846335b9530780364cc5
Tatya Hanum Pramudita <113042083+velottya@users.noreply.github.com>
2024-03-17 16:44:00 +0700

    blog, cover

--- resources/views/home/blog.blade.php
+++ resources/views/home/blog.blade.php
@@ -1,7 +1,7 @@
 @extends('home.layout')
 @section('title', 'Asesmen')
 @section('content')
-<section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('images/bg_1.jpg');">
+<section class="hero-wrap hero-wrap-2 js-fullheight" style="background-image: url('images/Screenshot 2024-02-27\ 205123.png');">
     <div class="overlay"></div>
     <div class="container">
       <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-center">
@@ -18,16 +18,16 @@
       <div class="row d-flex">
         <div class="col-md-4 d-flex ftco-animate">
          <div class="blog-entry justify-content-end">
-          <a href="blog-single.html" class="block-20" style="background-image: url('images/image_1.jpg');">
+          <a href="blog-single.html" class="block-20" style="background-image: url('images/image3.png');">
           </a>
           <div class="text">
            <div class="d-flex align-items-center mb-4 topp">
             <div class="one">
-             <span class="day">11</span>
+             <span class="day">2</span>
            </div>
            <div class="two">
-             <span class="yr">2020</span>
-             <span class="mos">September</span>
+             <span class="yr">2024</span>
+             <span class="mos">Februari</span>
            </div>
          </div>
          <h3 class="heading"><a href="#">"Virus" Topeng Malang Dari Polowijen Ditularkan ke Para Siswa</a></h3>
@@ -38,16 +38,16 @@
    </div>
    <div class="col-md-4 d-flex ftco-animate">
      <div class="blog-entry justify-content-end">
-      <a href="blog-single.html" class="block-20" style="background-image: url('images/image_2.jpg');">
+      <a href="blog-single.html" class="block-20" style="background-image: url('images/image6.png');">
       </a>
       <div class="text">
        <div class="d-flex align-items-center mb-4 topp">
         <div class="one">
-         <span class="day">11</span>
+         <span class="day">21</span>
        </div>
        <div class="two">
-         <span class="yr">2020</span>
-         <span class="mos">September</span>
+         <span class="yr">2024</span>
+         <span class="mos">Januari</span>
        </div>
      </div>
      <h3 class="heading"><a href="#">Pegiat Kampung Budaya Polowijen Bentuk Asosiasi Batik Malang </a></h3>
@@ -58,16 +58,16 @@
   </div>
   <div class="col-md-4 d-flex ftco-animate">
    <div class="blog-entry">
-    <a href="blog-single.html" class="block-20" style="background-image: url('images/image_3.jpg');">
+    <a href="blog-single.html" class="block-20" style="background-image: url('images/image4.png');">
     </a>
     <div class="text">
      <div class="d-flex align-items-center mb-4 topp">
       <div class="one">
-       <span class="day">11</span>
+       <span class="day">9</span>
      </div>
      <div class="two">
-       <span class="yr">2020</span>
-       <span class="mos">September</span>
+       <span class="yr">2023</span>
+       <span class="mos">November</span>
      </div>
    </div>
    <h3 class="heading"><a href="#">Kampung Budaya Polowijen Gelar Festival Topeng Malang</a></h3>
@@ -78,16 +78,16 @@
   </div>
   <div class="col-md-4 d-flex ftco-animate">
    <div class="blog-entry">
-    <a href="blog-single.html" class="block-20" style="background-image: url('images/image_4.jpg');">
+    <a href="blog-single.html" class="block-20" style="background-image: url('images/image1.jpg');">
     </a>
     <div class="text">
      <div class="d-flex align-items-center mb-4 topp">
       <div class="one">
-       <span class="day">11</span>
+       <span class="day">7</span>
      </div>
      <div class="two">
-       <span class="yr">2020</span>
-       <span class="mos">September</span>
+       <span class="yr">2023</span>
+       <span class="mos">Oktober</span>
      </div>
    </div>
    <h3 class="heading"><a href="#">Profil tokoh budaya menjaga kearifan lokal di Kampung Polowijen sebagai pembawa tradisi</a></h3>
@@ -98,7 +98,7 @@
   </div>
   <div class="col-md-4 d-flex ftco-animate">
    <div class="blog-entry">
-    <a href="blog-single.html" class="block-20" style="background-image: url('images/image_5.jpg');">
+    <a href="blog-single.html" class="block-20" style="background-image: url('images/galeri3-1.png');">
     </a>
     <div class="text">
      <div class="d-flex align-items-center mb-4 topp">
@@ -106,8 +106,8 @@
        <span class="day">11</span>
      </div>
      <div class="two">
-       <span class="yr">2020</span>
-       <span class="mos">September</span>
+       <span class="yr">2023</span>
+       <span class="mos">Agustus</span>
      </div>
    </div>
    <h3 class="heading"><a href="#">Menghidupkan Ekonomi Kreatif: Pasar Jajan Tradisional di Kampung Budaya Polowijen</a></h3>
@@ -118,16 +118,16 @@
   </div>
   <div class="col-md-4 d-flex ftco-animate">
    <div class="blog-entry">
-    <a href="blog-single.html" class="block-20" style="background-image: url('images/image_6.jpg');">
+    <a href="blog-single.html" class="block-20" style="background-image: url('images/GebyakWayangTopeng.png');">
     </a>
     <div class="text">
      <div class="d-flex align-items-center mb-4 topp">
       <div class="one">
-       <span class="day">11</span>
+       <span class="day">13</span>
      </div>
      <div class="two">
-       <span class="yr">2020</span>
-       <span class="mos">September</span>
+       <span class="yr">2023</span>
+       <span class="mos">Juni</span>
      </div>
    </div>
    <h3 class="heading"><a href="#">Pesona Seni dan Budaya Lokal: Keindahan Kampung Budaya Polowijen di Malang</a></h3>
@@ -144,9 +144,9 @@
           <li><a href="#">&lt;</a></li>
           <li class="active"><span>1</span></li>
           <li><a href="#">2</a></li>
-          <li><a href="#">3</a></li>
+          {{-- <li><a href="#">3</a></li>
           <li><a href="#">4</a></li>
-          <li><a href="#">5</a></li>
+          <li><a href="#">5</a></li> --}}
           <li><a href="#">&gt;</a></li>
         </ul>
       </div>
@@ -159,7 +159,7 @@
    <div class="container">
     <div class="row justify-content-center">
      <div class="col-md-12 text-center">
-      <div class="img"  style="background-image: url(images/bg_2.jpg);">
+      <div class="img"  style="background-image: url(images/Ask.jpg);">
        <div class="overlay"></div>
        <h2>KAMPUNG BUDAYA POLOWIJEN</h2>
        <p>Warisi Tradisi Lestarikan Budaya</p>
