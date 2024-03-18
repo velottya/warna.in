fe75695b551eeb39fb55e390f5577fd98bd3a19b
Rizky Puspitaa <rizkypuspitaa19@gmail.com>
2024-03-19 01:43:18 +0700

    ...

--- resources/views/home/layout.blade.php
+++ resources/views/home/layout.blade.php
@@ -65,49 +65,48 @@
             <div class="row mb-5">
                 <div class="col-md pt-5">
                     <div class="ftco-footer-widget pt-md-5 mb-4">
-                        <h2 class="ftco-heading-2">About</h2>
-                        <p>Far far away, behind the word mountains, far from the countries Vokalia and Consonantia, there live the blind texts.</p>
+                        <h2 class="ftco-heading-2"> Kampung Budaya Polowijen </h2>
+                        <p>Kampung Edukasi sebagai Sarana Pelestarian Budaya di Kota Malang</p>
                         <ul class="ftco-footer-social list-unstyled float-md-left float-lft">
-                            <li class="ftco-animate"><a href="#"><span class="fa fa-twitter"></span></a></li>
-                            <li class="ftco-animate"><a href="#"><span class="fa fa-facebook"></span></a></li>
-                            <li class="ftco-animate"><a href="#"><span class="fa fa-instagram"></span></a></li>
+                            <li class="ftco-animate"><a href="https://twitter.com/PemkotMalang"><span class="fa fa-twitter"></span></a></li>
+                            <li class="ftco-animate"><a href="https://m.facebook.com/people/Kampung-Budaya-Polowijen/100023993563201/?locale=ms_MY"><span class="fa fa-facebook"></span></a></li>
+                            <li class="ftco-animate"><a href="https://www.instagram.com/kampungbudayapolowijen?utm_source=ig_web_button_share_sheet&igsh=ZDNlZDc0MzIxNw=="><span class="fa fa-instagram"></span></a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-md pt-5 border-left">
                     <div class="ftco-footer-widget pt-md-5 mb-4 ml-md-5">
-                        <h2 class="ftco-heading-2">Infromation</h2>
+                        <h2 class="ftco-heading-2">Menu</h2>
                         <ul class="list-unstyled">
-                            <li><a href="#" class="py-2 d-block">Online Enquiry</a></li>
-                            <li><a href="#" class="py-2 d-block">General Enquiries</a></li>
-                            <li><a href="#" class="py-2 d-block">Booking Conditions</a></li>
-                            <li><a href="#" class="py-2 d-block">Privacy and Policy</a></li>
-                            <li><a href="#" class="py-2 d-block">Refund Policy</a></li>
-                            <li><a href="#" class="py-2 d-block">Call Us</a></li>
+                            <li><a href="/" class="py-2 d-block">Homepage</a></li>
+                            <li><a href="/about" class="py-2 d-block">About KBP</a></li>
+                            <li><a href="/sentra" class="py-2 d-block">Sentra</a></li>
+                            <li><a href="/galeri" class="py-2 d-block">Galeri</a></li>
+                            <li><a href="/blog" class="py-2 d-block">Blog</a></li>
+                            <li><a href="/contact" class="py-2 d-block">Contact</a></li>
+                            <li><a href="/login" class="py-2 d-block">Log In</a></li>
                         </ul>
                     </div>
                 </div>
                 <div class="col-md pt-5 border-left">
                     <div class="ftco-footer-widget pt-md-5 mb-4">
-                        <h2 class="ftco-heading-2">Experience</h2>
+                        <h2 class="ftco-heading-2">Informasi</h2>
                         <ul class="list-unstyled">
-                            <li><a href="#" class="py-2 d-block">Adventure</a></li>
-                            <li><a href="#" class="py-2 d-block">Hotel and Restaurant</a></li>
-                            <li><a href="#" class="py-2 d-block">Beach</a></li>
-                            <li><a href="#" class="py-2 d-block">Nature</a></li>
-                            <li><a href="#" class="py-2 d-block">Camping</a></li>
-                            <li><a href="#" class="py-2 d-block">Party</a></li>
-                        </ul>
+                            <li><a href="#" class="py-2 d-block">Sejarah</a></li>
+                            <li><a href="#" class="py-2 d-block">Paket Kegiatan</a></li>
+                            <li><a href="#" class="py-2 d-block">Galeri dan Video</a></li>
+                            <li><a href="#" class="py-2 d-block">Berita</a></li>
+                            <li><a href="#" class="py-2 d-block">Produk</a></li>                        </ul>
                     </div>
                 </div>
                 <div class="col-md pt-5 border-left">
                     <div class="ftco-footer-widget pt-md-5 mb-4">
-                        <h2 class="ftco-heading-2">Have a Questions?</h2>
+                        <h2 class="ftco-heading-2">Kontak</h2>
                         <div class="block-23 mb-3">
                             <ul>
-                                <li><span class="icon fa fa-map-marker"></span><span class="text">203 Fake St. Mountain View, San Francisco, California, USA</span></li>
-                                <li><a href="#"><span class="icon fa fa-phone"></span><span class="text">+2 392 3929 210</span></a></li>
-                                <li><a href="#"><span class="icon fa fa-paper-plane"></span><span class="text">info@yourdomain.com</span></a></li>
+                                <li><span class="icon fa fa-map-marker"></span><span class="text">Jl. Cakalang, RT.3/RW.2, Polowijen, Kec. Blimbing, Kota Malang, Jawa Timur</span></li>
+                                <li><a href="#"><span class="icon fa fa-phone"></span><span class="text">+62 815-5181-303</span></a></li>
+                                <li><a href="#"><span class="icon fa fa-paper-plane"></span><span class="text">kampungbudayapolowijen@gmail.com</span></a></li>
                             </ul>
                         </div>
                     </div>
@@ -117,7 +116,7 @@
                 <div class="col-md-12 text-center">
 
                     <p><!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. -->
-                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> All rights reserved | This template is made with <i class="fa fa-heart" aria-hidden="true"></i> by <a href="https://colorlib.com" target="_blank">Colorlib</a>
+                        Copyright &copy;<script>document.write(new Date().getFullYear());</script> | KELOMPOK 7 
                         <!-- Link back to Colorlib can't be removed. Template is licensed under CC BY 3.0. --></p>
                     </div>
                 </div>
