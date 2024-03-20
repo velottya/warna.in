b376c73f7b794c9f8758ad1320d61319767498d4
Harunsatr <harunsatr36@gmail.com>
2024-03-20 10:02:54 +0700

    contact

--- resources/views/home/contact.blade.php
+++ resources/views/home/contact.blade.php
@@ -56,9 +56,13 @@
   </div>
   </section>
 
+
   <section class="ftco-section contact-section ftco-no-pt">
     <div class="container">
       <div class="row block-9">
+        <div class="col-md-6 order-md-last d-flex" style="margin-top:30px;">
+            <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3951.671669147024!2d112.64081027476755!3d-7.929319392094449!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x2dd629f2b6c595ff%3A0xd8ab802f6677086e!2sKampung%20Budaya%20Polowijen!5e0!3m2!1sid!2sid!4v1710901895732!5m2!1sid!2sid" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div>
         <div class="col-md-6 order-md-last d-flex">
           <form action="#" class="bg-light p-5 contact-form">
             <div class="form-group">
@@ -77,12 +81,8 @@
               <input type="submit" value="Send" class="btn btn-primary py-3 px-5">
             </div>
           </form>
-
         </div>
 
-        <div class="col-md-6 d-flex">
-         <div id="map" class="bg-white"></div>
-       </div>
      </div>
    </div>
   </section>
