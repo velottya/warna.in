d6c07e68628938f3a611de0971613528d04fbf55
Rizky Puspitaa <rizkypuspitaa19@gmail.com>
2024-03-27 13:05:51 +0700

    ...

--- resources/views/home/sentra/sambang4.blade.php
+++ resources/views/home/sentra/sambang4.blade.php
@@ -2,20 +2,7 @@
 @section('title', 'Sentra')
 @section('content')
 
-<section class="hero-wrap hero-wrap-2 js-fullheight"
-      style="background-image: url('images/4.jpg');">
-      <div class="overlay"></div>
-      <div class="container">
-        <div class="row no-gutters slider-text js-fullheight align-items-end justify-content-center">
-          <div class="col-md-9 ftco-animate pb-5 text-center">
-            <p class="breadcrumbs"><span class="mr-2"><a href="index.html">Home <i
-                    class="fa fa-chevron-right"></i></a></span> <span>Sentra <i class="fa fa-chevron-right"></i></span>
-            </p>
-            <h1 class="mb-0 bread">Sambang Kampung</h1>
-          </div>
-        </div>
-      </div>
-  </section>
+
   <section class="ftco-section services-section">
   <div class="container">
     <div class="row d-flex">
